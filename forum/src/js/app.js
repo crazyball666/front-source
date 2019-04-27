73f3dac33aba7818dde6320aed022ff546436cd2
crazyball <774028406@qq.com>
2019-04-27 22:42:12 +0800

    build

--- forum/src/js/app.js
+++ forum/src/js/app.js
@@ -2,6 +2,10 @@ import "../scss/app.scss";
 import Vue from "vue";
 import router from "./router/index";
 import App from "./component/App.vue";
+
+window.onclick = function () {
+  alert(111)
+}
 //
 // alert(111);
 // Vue.use(router);
