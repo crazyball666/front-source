701c82f51f9fa8f04f63eadc300df94b764e3fb9
sujiahao <774028406@qq.com>
2020-08-02 15:33:17 +0800

    build admin

--- admin/src/app.js
+++ admin/src/app.js
@@ -10,7 +10,7 @@ import types from "./store/mutation-type"
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-    let accessToken = localStorage.getItem("accessToken")
+    let accessToken = localStorage.getItem("access_token")
     let user = JSON.parse(localStorage.getItem("userInfo"))
     if (!user || !accessToken) {
       // location.href = "//user.crazyball.xyz/login?redirectURL=http://localhost:9001/verify?orginURL=http://localhost:9001/"
